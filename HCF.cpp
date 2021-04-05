085cd93c3bd0f0835ce0d4bab9d94341ae0b3c3e
Pallav <69533542+PallavRastogi@users.noreply.github.com>
2021-04-05 14:55:51 +0530

    Create HCF.cpp
    C++ source code

---
+++ HCF.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+int euclid (int, int);
+
+int main() {
+
+    int a, b;
+
+    cout<<"Enter First Number"<<endl;
+
+    cin>>a;
+
+    
+
+    cout<<"Enter Second Number"<<endl;
+
+    cin>>b;
+
+    cout<<"\n- - - - - - - -\n";
+
+    cout<<"The HCF of "<<a<<" and "<<b<<" is "<<euclid(a, b);
+
+    return 0;
+
+}
+
+int euclid(int a, int b){
+
+    if(a<b){
+
+        int temp = b;
+
+        b = a;
+
+        a = temp;
+
+    }
+
+    
+
+    if(a%b == 0){
+
+        return b;
+
+    }
+
+    
+
+    else{
+
+        euclid(b, (a%b));
+
+    }
+
+    
+
+}
+
